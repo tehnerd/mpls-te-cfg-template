664c81033deea81d33ba75d573af4ae3ddb89a8e
tehnerd <ns.hando@gmail.com>
2014-06-27 15:27:29 +0400

    changelog:     *rewriting templates to use jinja instead of m4...

---
+++ create-te-tunnel.py
@@ -0,0 +1,52 @@
+import jinja2
+import sys
+import sqlite3
+from string import join
+
+
+
+
+class CfgContext(object):
+    def __init__(self):
+        self.cfg_dict = dict()
+        #need to check jinja2 docs about something more usefull
+        self.TEMPLATE_DIR = './'
+        self.env = jinja2.Environment(loader=jinja2.FileSystemLoader(self.TEMPLATE_DIR))
+        self.DBNAME='./routers.db'
+        self.connection = sqlite3.connect(self.DBNAME)
+        self.cursor = self.connection.cursor()
+
+    
+    def cfg_from_cli(self,cfg_line):
+        if len(cfg_line) < 4:
+            print("arguments must be: <vendor> <descr/lsp name> <tunnel_num> <hosts>...")
+            sys.exit(-1)
+        self.cfg_dict['vendor'] = cfg_line[0]
+        self.cfg_dict['lsp_name'] = cfg_line[1]
+        self.cfg_dict['tunnel_num'] = cfg_line[2]
+        self.cfg_dict['nodes'] = list()
+        node_id = 3
+        while node_id < len(cfg_line):
+            self.cursor.execute("select r_ip from rdb where r_name = ?",
+                        (cfg_line[node_id],))
+            node_ip = self.cursor.fetchall()[0]
+            self.cfg_dict['nodes'].extend(node_ip)
+            node_id += 1
+
+    def render_template(self):
+        template_name = join((self.TEMPLATE_DIR,
+                              "mpls-te-cfg.jinja"),'')
+        template = self.env.get_template(template_name)
+        print template.render(cfg_dict = self.cfg_dict)
+
+def main():
+    cfg_context = CfgContext()
+    cfg_context.cfg_from_cli(sys.argv[1:])
+    print(cfg_context.cfg_dict)
+    cfg_context.render_template()
+
+
+
+
+if __name__ == '__main__':
+    main()
